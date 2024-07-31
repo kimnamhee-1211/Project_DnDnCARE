40e525c47ef05f57a6cc69f150dd287ef58dcc93
kimnamhee <namhee6741@naver.com>
2024-08-01 02:23:01 +0900

    공동간병

--- src/main/java/com/kh/dndncare/joinMaching/controller/JoinMachingController.java
+++ src/main/java/com/kh/dndncare/joinMaching/controller/JoinMachingController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.kh.dndncare.joinMaching.model.service.JoinMachingService;
+import com.kh.dndncare.joinMaching.model.vo.Hospital;
 
 @Controller
 public class JoinMachingController {
@@ -15,12 +17,38 @@ public class JoinMachingController {
 	
 	
 	@GetMapping("joinMachingMainView.jm")
-	public String joinMachingMainView(Model model) {
-		model.addAttribute("currentPage", 1);
+	public String joinMachingMainView() {
+		
 		return "joinMachingMain";
 	}
 	
-	
+	@GetMapping("joinMaching.jm")
+	public String joinMachingMain(@ModelAttribute Hospital ho, Model model) {
+		
+		model.addAttribute("ho", ho);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		return "joinMaching";
+	}
 
 	
 	
