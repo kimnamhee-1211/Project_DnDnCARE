e717e634d9cfd803dbc7521f38c584310f4b996f
user1 <user1@DESKTOP-D4BGNTS>
2024-07-31 19:01:14 +0900

    공동간병 메인페이지 끝

--- src/main/java/com/kh/dndncare/joinMaching/controller/JoinMachingController.java
+++ src/main/java/com/kh/dndncare/joinMaching/controller/JoinMachingController.java
@@ -2,6 +2,7 @@ package com.kh.dndncare.joinMaching.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.kh.dndncare.joinMaching.model.service.JoinMachingService;
@@ -14,8 +15,8 @@ public class JoinMachingController {
 	
 	
 	@GetMapping("joinMachingMainView.jm")
-	public String joinMachingMainView() {
-			
+	public String joinMachingMainView(Model model) {
+		model.addAttribute("currentPage", 1);
 		return "joinMachingMain";
 	}
 	
