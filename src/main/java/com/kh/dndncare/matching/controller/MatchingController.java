3e69a3d4ae930eb5753c5d0656c1aaff888edac7
kim-ki-ryong <rlarlfyd1258@naver.com>
2024-08-02 17:52:38 +0900

    아자아자

--- src/main/java/com/kh/dndncare/matching/controller/MatchingController.java
+++ src/main/java/com/kh/dndncare/matching/controller/MatchingController.java
@@ -2,6 +2,7 @@ package com.kh.dndncare.matching.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MatchingController {
@@ -9,5 +10,10 @@ public class MatchingController {
 	@GetMapping("publicMatching.mc")
 	public String publicMatchingView() {
 		return "publicMatching";
+	}
+	
+	@PostMapping("publicMatching2.mc")
+	public String publicMatching2() {
+		return "publicMatching2";
 	}
 }
