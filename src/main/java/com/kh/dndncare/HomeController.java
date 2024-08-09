9ff16f93a4cfc9ee4cda7635fdf2a4101d0d0ce6
소영 <@192.168.219.126>
2024-08-09 23:46:55 +0900

    네이버 간편로그인 적용, 아이디없으면 회원가입으로넘어가고 있으면 바로로그인을 적용해보자

--- src/main/java/com/kh/dndncare/HomeController.java
+++ src/main/java/com/kh/dndncare/HomeController.java
@@ -1,7 +1,12 @@
 package com.kh.dndncare;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
@@ -12,8 +17,21 @@ public class HomeController {
 		return "home";
 	}
 	
-	@GetMapping("naver.do")
+	public String generateState()
+	{
+		SecureRandom random = new SecureRandom();
+		return new BigInteger(130, random).toString(32);
+	}
+	
+	@GetMapping("naver.lo")
 	public String naver() {
 		return "naverTest";
 	}
+	
+	@GetMapping("callback.lo")
+	public String callback() {
+		return "callback";
+	}
+	
+
 }
