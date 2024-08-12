cac7427be604d087c35a0bb3ad5ddab3b71399bd
kim-ki-ryong <rlarlfyd1258@naver.com>
2024-08-12 16:17:26 +0900

    하하

--- src/main/java/com/kh/dndncare/chating/controller/ChatingController.java
+++ src/main/java/com/kh/dndncare/chating/controller/ChatingController.java
@@ -1,5 +1,8 @@
 package com.kh.dndncare.chating.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ChatingController {
 
 }
--- src/main/java/com/kh/dndncare/common/config/TemplateResolverConfig.java
+++ src/main/java/com/kh/dndncare/common/config/TemplateResolverConfig.java
@@ -98,6 +98,10 @@ public class TemplateResolverConfig {
 		return dotCh;
 	}
 	
+
+	
+	
+	
 	
 	
 	
