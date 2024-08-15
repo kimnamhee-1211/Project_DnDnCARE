f190e08ef69d359b8eb4f29e3a16eccb88d68a9c
소영 <@DESKTOP-FIR4UO5>
2024-08-15 20:11:41 +0900

    주석처리 인터셉터,이미지파일,로그백!

--- src/main/java/com/kh/dndncare/common/config/WebMvcConfig.java
+++ src/main/java/com/kh/dndncare/common/config/WebMvcConfig.java
@@ -9,25 +9,25 @@ import com.kh.dndncare.common.interceptor.CheckLoginInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
-	
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		// 임시 경로로 테스트 수행 후 공유 폴더로 변경할 것
-		registry.addResourceHandler("/**").addResourceLocations("file:///c:/uploadFinalFiles/", "classpath:static/", "\\\\192.168.40.37\\sharedFolder/dndnCare/");  
-		WebMvcConfigurer.super.addResourceHandlers(registry);
-	}
-	
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-
-		
-		//기본적인 로그인 인터셉터 넣기
-		registry.addInterceptor(new CheckLoginInterceptor())
-		.addPathPatterns("/myInfo.me","/patientMain.me","/caregiverMain.me","/moreCaregiverInfo.me","/joinMatchingMainView.jm","/communityBoardList.bo");
-		
-		
-		
-		WebMvcConfigurer.super.addInterceptors(registry);
-	}
-	
+//	
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		// 임시 경로로 테스트 수행 후 공유 폴더로 변경할 것
+//		registry.addResourceHandler("/**").addResourceLocations("file:///c:/uploadFinalFiles/", "classpath:static/", "\\\\192.168.40.37\\sharedFolder/dndnCare/");  
+//		WebMvcConfigurer.super.addResourceHandlers(registry);
+//	}
+//	
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry) {
+//
+//		
+//		//기본적인 로그인 인터셉터 넣기
+//		registry.addInterceptor(new CheckLoginInterceptor())
+//		.addPathPatterns("/myInfo.me","/patientMain.me","/caregiverMain.me","/moreCaregiverInfo.me","/joinMatchingMainView.jm","/communityBoardList.bo");
+//		
+//		
+//		
+//		WebMvcConfigurer.super.addInterceptors(registry);
+//	}
+//	
 }
