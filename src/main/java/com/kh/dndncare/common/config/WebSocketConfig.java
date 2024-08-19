cac9b94065342082933add921147b50829ad57ec
kim-ki-ryong <rlarlfyd1258@naver.com>
2024-08-19 10:32:32 +0900

    웹소켓설정 살짝 수정

--- src/main/java/com/kh/dndncare/common/config/WebSocketConfig.java
+++ src/main/java/com/kh/dndncare/common/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.kh.dndncare.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -22,7 +23,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 	    registry.addEndpoint("/ws-stomp") // 클라이언트가 WebSocket에 연결할 엔드포인트 설정
-	    		.setAllowedOriginPatterns("*")
+	    		.setAllowedOriginPatterns("http://192.168.40.*")
 	            .withSockJS(); // SockJS를 지원하여 낮은 버전의 브라우저도 WebSocket을 사용할 수 있도록 설정
 	}
+	
+	
 }
