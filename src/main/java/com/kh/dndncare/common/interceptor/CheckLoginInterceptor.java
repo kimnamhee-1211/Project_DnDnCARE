408ce494e32f9b7cdd1e7a50f11db975c2483598
JONGGYU94 <gowhdrb@naver.com>
2024-08-23 17:49:08 +0900

    리뷰펭지 수정

--- src/main/java/com/kh/dndncare/common/interceptor/CheckLoginInterceptor.java
+++ src/main/java/com/kh/dndncare/common/interceptor/CheckLoginInterceptor.java
@@ -41,25 +41,29 @@ public class CheckLoginInterceptor implements HandlerInterceptor{
 			response.setContentType("text/html; charset=UTF-8");
 			response.getWriter().write("<script> alert('"+ msg +"'); location.href='home.do';</script>");		
 			return false;
-		}else if(loginUser.getMemberCategory() == "C"){
-			System.out.println("ㅎ간병인로그인시 인터셉터 체크");
-			//간병인에서 환자페이지는 막도록 하자
-			if(url.contains("moreCaregiverInfo.me") ||url.contains("patientMain.me") || url.contains("joinMatchingMainView.jm") || url.contains("joinMatching.jm")|| url.contains("/joinMatchingEnrollView.jm")) {
-				msg = "환자 페이지는 이동할 수 없습니다";
-				response.setContentType("text/html; charset=UTF-8");
-				response.getWriter().write("<script> alert('"+ msg +"'); location.href='home.do';</script>");
-				return false;
-			}
-		}else if(loginUser.getMemberCategory() == "P") {
-			System.out.println("환자로그인시 인터셉터 체크");
-			//환자에서 간병인페이지는 막도록 하자
-			if(url.contains("caregiverMain.me") ||url.contains("myInfoMatchingHistory.me")||url.contains("myInfoMatchingReview.me")||url.contains("moreWorkInfo.me")) {
-				msg = "간병인 페이지는 이동할 수 없습니다";
-				response.setContentType("text/html; charset=UTF-8");
-				response.getWriter().write("<script> alert('"+ msg +"'); location.href='home.do';</script>");
-				return false;
-			}
+		}else {
+		
+			if(loginUser.getMemberCategory() == "C"){
+				System.out.println("ㅎ간병인로그인시 인터셉터 체크");
+				//간병인에서 환자페이지는 막도록 하자
+				if(url.contains("moreCaregiverInfo.me") ||url.contains("patientMain.me") || url.contains("joinMatchingMainView.jm") || url.contains("joinMatching.jm")|| url.contains("/joinMatchingEnrollView.jm")) {
+					msg = "환자 페이지는 이동할 수 없습니다";
+					response.setContentType("text/html; charset=UTF-8");
+					response.getWriter().write("<script> alert('"+ msg +"'); location.href='home.do';</script>");
+					return false;
+				}
+			}else if(loginUser.getMemberCategory() == "P") {
+				System.out.println("환자로그인시 인터셉터 체크");
+				//환자에서 간병인페이지는 막도록 하자
+				if(url.contains("caregiverMain.me") ||url.contains("myInfoMatchingHistory.me")||url.contains("myInfoMatchingReview.me")||url.contains("moreWorkInfo.me")) {
+					msg = "간병인 페이지는 이동할 수 없습니다";
+					response.setContentType("text/html; charset=UTF-8");
+					response.getWriter().write("<script> alert('"+ msg +"'); location.href='home.do';</script>");
+					return false;
+				}
 			
+			}
+		
 		}
 		return HandlerInterceptor.super.preHandle(request, response, handler);
 	}
