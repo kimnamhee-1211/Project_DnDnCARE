0ee493b2bf905d7248d4733ae2b18122aaebe7ff
JONGGYU94 <gowhdrb@naver.com>
2024-08-23 17:09:42 +0900

    인터셉터까지 그냥 넣음

--- src/main/java/com/kh/dndncare/common/interceptor/CheckLoginInterceptor.java
+++ src/main/java/com/kh/dndncare/common/interceptor/CheckLoginInterceptor.java
@@ -18,19 +18,20 @@ public class CheckLoginInterceptor implements HandlerInterceptor{
 		HttpSession session = request.getSession();
 		Member loginUser = (Member)session.getAttribute("loginUser");
 		
+		String url = request.getRequestURI();
+		
+		String msg = null;
 		if(loginUser == null) {
-			String url = request.getRequestURI();
 			
-			String msg = null;
 			if(url.contains("moreWorkInfo.me") || url.contains("")) {	//여긴 간병인 로그인페이지
 				msg = "로그인 후 이용하세요";
 				response.setContentType("text/html; charset=UTF-8");
-				response.getWriter().write("<script> alert('"+ msg +"'); location.href='careGiver.me';</script>");
+				response.getWriter().write("<script> alert('"+ msg +"'); location.href='home.do';</script>");
 				return false;
 			}else if(url.contains("moreCaregiverInfo.me") || url.contains("joinMatchingMainView.jm")) {	//여긴 환자페이지로 이동시키자
 				msg = "로그인 후 이용하세요";
 				response.setContentType("text/html; charset=UTF-8");
-				response.getWriter().write("<script> alert('"+ msg +"'); location.href='patient.me';</script>");
+				response.getWriter().write("<script> alert('"+ msg +"'); location.href='home.do';</script>");
 				return false;
 			}else {
 				msg = "로그인 후 확인 가능합니다";
@@ -39,6 +40,24 @@ public class CheckLoginInterceptor implements HandlerInterceptor{
 			response.setContentType("text/html; charset=UTF-8");
 			response.getWriter().write("<script> alert('"+ msg +"'); location.href='home.do';</script>");		
 			return false;
+		}else if(loginUser.getMemberCategory() == "C"){
+			//간병인에서 환자페이지는 막도록 하자
+			if(url.contains("moreCaregiverInfo.me") ||url.contains("patientMain.me") || url.contains("joinMatchingMainView.jm") || url.contains("joinMatching.jm")|| url.contains("/joinMatchingEnrollView.jm")) {
+				msg = "환자 페이지는 이동할 수 없습니다";
+				response.setContentType("text/html; charset=UTF-8");
+				response.getWriter().write("<script> alert('"+ msg +"'); location.href='home.do';</script>");
+				return false;
+			}
+			
+		}else if(loginUser.getMemberCategory() == "P") {
+			//환자에서 간병인페이지는 막도록 하자
+			if(url.contains("caregiverMain.me") ||url.contains("myInfoMatchingHistory.me")||url.contains("myInfoMatchingReview.me")||url.contains("moreWorkInfo.me")) {
+				msg = "간병인 페이지는 이동할 수 없습니다";
+				response.setContentType("text/html; charset=UTF-8");
+				response.getWriter().write("<script> alert('"+ msg +"'); location.href='home.do';</script>");
+				return false;
+			}
+			
 		}
 		return HandlerInterceptor.super.preHandle(request, response, handler);
 	}
