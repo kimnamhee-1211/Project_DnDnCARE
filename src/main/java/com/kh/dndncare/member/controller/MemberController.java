0dc2d9e48a5fa0fce698733189cc6a0545cdb7aa
kimnamhee <namhee6741@naver.com>
2024-09-02 14:59:14 +0900

    Merge branch 'backup' of https://github.com/JONGGYU94/dndncare.git into backup

--- src/main/java/com/kh/dndncare/member/controller/MemberController.java
+++ src/main/java/com/kh/dndncare/member/controller/MemberController.java
@@ -72,7 +72,7 @@ import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@SessionAttributes({ "loginUser", "tempMemberCategory", "enrollmember" })
+@SessionAttributes({ "loginUser", "tempMemberCategory" })
 @Controller
 public class MemberController {
 
